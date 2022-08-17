3ee7d2f8de16f86f9e2719324224a51396304a68
bianca <cupcakeeeee17@gmail.com>
2022-08-17 10:08:43 -0600

    create cards js

--- main.js
+++ main.js
@@ -5,3 +5,34 @@ async function fetchCategories() {
   let categories = await response.json();
   return categories;
 }
+
+function createCards() {
+  fetchCategories().then((categories) => {
+    for (let category of categories.communityCategories) {
+      const card = document.createRange().createContextualFragment(/*html*/ `
+        <div class="card">
+          <div
+            style="
+              background-image: url('https://storage.googleapis.com/bucket-larnu/media/business/145/images/YS4GQW1Q.png');
+              background-size: contain;
+              border-radius: 30px 30px 0px 0px;
+              height: 140px;
+            "
+          >
+            <img
+              src="https://storage.googleapis.com/bucket-larnu/media/business/123/images/NHNE8PPG.png"
+            />
+          </div>
+          <div>
+            <h3>SEO</h3>
+            <p>Total Quizes: 34</p>
+            <p>Users: 130</p>
+            <a href="https://www.larnu.com">Go to LarnU</a>
+          </div>
+        </div>
+      `);
+    }
+  });
+}
+
+createCards();
